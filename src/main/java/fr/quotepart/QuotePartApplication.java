96209e1069ffedbe8da2c915244d4a855e0126ac
Iannis Vasselet <iannisvasselet@gmail.com>
2026-06-29 13:03:03 +0200

    build: passage à Spring Boot (data-jpa, flyway, testcontainers)

---
+++ src/main/java/fr/quotepart/QuotePartApplication.java
@@ -0,0 +1,12 @@
+package fr.quotepart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotePartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotePartApplication.class, args);
+    }
+}
